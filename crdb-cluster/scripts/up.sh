a7058b5f6e769aea77b82a6db23c618cb037c329
Martial Salgues <martial.salgues@gmail.com>
2022-07-20 23:06:21 +0200

    update working directory

--- crdb-cluster/scripts/up.sh
+++ crdb-cluster/scripts/up.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-WORK_DIR=./crdb-cluster/
+# Change working directory :
+cd crdb-cluster/ > /dev/null
 
-if [[ "$(ls -a ./certs)" ]] && [[ $1 == "sql" ]]	; then
+if [[ "$(ls ./certs)" ]] && [[ $1 == "sql" ]]	; then
   if [[ $2 == "backup" ]] ; then
     echo "lauching cockroach and open a console in backup mode..."
     # in backup mode, mount the backup folder to the docker roach-0 container.
     # if we want to restore a backup, we can open a shell in the docker roach-0 container to copy the backup from backup/ to cockroach-data/extern/backup
-    docker compose -f ${WORK_DIR}/docker-compose.yml -f ${WORK_DIR}/docker-compose.backup.yml up --no-start
-    docker compose start roach-0
+    docker compose -f docker-compose.yml -f docker-compose.backup.yml up --no-start
 
-    sleep 5
+    docker compose start roach-0
 
     docker compose start roach-1
     docker compose start roach-2
@@ -19,13 +19,12 @@ if [[ "$(ls -a ./certs)" ]] && [[ $1 == "sql" ]]	; then
     sleep 5
 
     # Open SQL console to backup OR restore, then remove container
-    docker compose -f ${WORK_DIR}/docker-compose.shell.yml run -it --rm --entrypoint='./cockroach sql' roach-shell
+    docker compose -f docker-compose.shell.yml run -it --rm --entrypoint='./cockroach sql' roach-shell
   else
     echo "lauching cockroach and open a console..."
-    docker compose -f ${WORK_DIR}/docker-compose.yml up --no-start
-    docker compose start roach-0
+    docker compose -f docker-compose.yml up --no-start
 
-    sleep 5
+    docker compose start roach-0
 
     docker compose start roach-1
     docker compose start roach-2
@@ -33,41 +32,40 @@ if [[ "$(ls -a ./certs)" ]] && [[ $1 == "sql" ]]	; then
 
     sleep 5
 
-    docker compose -f ${WORK_DIR}/docker-compose.shell.yml run -it --rm --entrypoint='./cockroach sql' roach-shell
+    docker compose -f docker-compose.shell.yml run -it --rm --entrypoint='./cockroach sql' roach-shell
   fi
 
-elif [[ "$(ls -a ./certs)" ]]; then
+elif [[ "$(ls ./certs)" ]]; then
     echo "Certificates found, lauching cockroach..."
-    docker compose -f ${WORK_DIR}/docker-compose.yml up --no-start
-    docker compose start roach-0
+    docker compose -f docker-compose.yml up --no-start
 
-    sleep 5
+    docker compose start roach-0
 
     docker compose start roach-1
     docker compose start roach-2
     docker compose start lb
 else
     echo "Certificates not found, initialise cockroach..."
-    docker compose -f ${WORK_DIR}/docker-compose.yml -f ${WORK_DIR}/docker-compose.init.yml up --no-start
+    docker compose -f docker-compose.yml -f docker-compose.init.yml up --no-start
     # Generate certificates :
     docker compose start roach-cert
 
-    sleep 20 # Wait for certificates to be generated
+    sleep 5 # Wait for certificates to be generated
 
     docker compose start roach-0
 
-    sleep 5
+    # sleep 1
 
     docker compose start roach-1
     docker compose start roach-2
     docker compose start lb
 
-    sleep 10 # Wait for cockroach to be ready
+    # sleep 1 # Wait for cockroach to be ready
 
     # Initialise cockroach cluster :
     docker compose start roach-init
 
-    sleep 10
+    sleep 5
 
     # Retrieve certificates :
     docker cp roach-cert:/.cockroach-certs/ca.crt ./certs/
